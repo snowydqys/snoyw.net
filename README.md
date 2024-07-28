cc8e80a8b3389661574a14ba622980c25c772205
6gh <47262177+6gh@users.noreply.github.com>
2024-07-28 04:18:06 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# snoyw.net Landing Page
+
+The website on [snoyw.net](https://snoyw.net). Used as my personal _Linktree_-like site.
---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        mdi: ["web", "email"],
+        "fa6-brands": [
+          "discord",
+          "youtube",
+          "x-twitter",
+          "reddit-alien",
+          "github",
+          "twitch",
+          "steam",
+        ],
+      },
+    }),
+  ],
+});
