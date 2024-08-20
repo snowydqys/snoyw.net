8eef8d34e5c69667f8a3e26474e4d1a135d6e56f
6gh <47262177+6gh@users.noreply.github.com>
2024-08-20 04:59:05 -0400

    chore: update deps and add partytown + add: self-hosted tracking link
    - tracking service is my own Shynet instance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    icon({
-      include: {
-        mdi: ["web", "email"],
-        "fa6-brands": [
-          "discord",
-          "youtube",
-          "x-twitter",
-          "reddit-alien",
-          "github",
-          "twitch",
-          "steam",
-        ],
-        "simple-icons": ["anilist"],
-        twemoji: ["paw-prints", "snowflake"],
-      },
-    }),
-  ],
+  integrations: [tailwind(), icon({
+    include: {
+      mdi: ["web", "email", "close"],
+      "fa6-brands": ["discord", "youtube", "x-twitter", "reddit-alien", "github", "twitch", "steam"],
+      "simple-icons": ["anilist"],
+      twemoji: ["paw-prints", "snowflake"]
+    }
+  }), partytown()],
   image: {
-    domains: ["cdn.snoyw.net"],
-  },
+    domains: ["cdn.snoyw.net"]
+  }
 });
