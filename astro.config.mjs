a34d1637a0bf0fe86a25bbcdb867f28004238eb9
6gh <47262177+6gh@users.noreply.github.com>
2024-11-08 03:13:07 -0500

    update analytics to use umami + remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,28 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon({
-    include: {
-      mdi: ["web", "email", "close"],
-      "fa6-brands": ["discord", "youtube", "x-twitter", "reddit-alien", "github", "twitch", "steam"],
-      "simple-icons": ["anilist"],
-      twemoji: ["paw-prints", "snowflake"]
-    }
-  }), partytown()],
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        mdi: ["web", "email", "close"],
+        "fa6-brands": [
+          "discord",
+          "youtube",
+          "x-twitter",
+          "reddit-alien",
+          "github",
+          "twitch",
+          "steam",
+        ],
+        "simple-icons": ["anilist"],
+        twemoji: ["paw-prints", "snowflake"],
+      },
+    }),
+  ],
   image: {
-    domains: ["cdn.snoyw.net"]
-  }
+    domains: ["cdn.snoyw.net"],
+  },
 });
